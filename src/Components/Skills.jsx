e0a358e3e3ee8f4e66ff315056c8096098910e52
Brandon <delleore1@gmail.com>
2025-07-15 23:11:33 -0500

    feat: :sparkles: Avances en el componente skills

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -4,6 +4,55 @@ import '../styles/skills.css'
 const Skills = () => {
    return (
     <main className="main_skills">
+            <div className="skills_container">
+                <div className="skills1">
+                        <div className="C_skills1">
+                                <div className="C_skills1_one">
+                                        <div className="C_pic">
+
+                                        </div>
+                                        <div className="C_name">
+                                                
+                                        </div>
+                                </div>
+                                <div className="C_skills1_two">
+
+                                </div>
+                        </div>
+                </div>
+                <div className="skills2">
+                        <div className="C_skills2">
+                                <div className="C_skills2_one">
+                                        <div className="C_pic">
+
+                                        </div>
+                                        <div className="C_name">
+                                                
+                                        </div>                                        
+                                </div>
+                                <div className="C_skills2_two">
+
+                                </div>
+                        </div>
+                </div>
+                <div className="skills3">
+                        <div className="C_skills3">
+                                <div className="C_skills3_one">
+                                        <div className="C_pic">
+
+                                        </div>
+                                        <div className="C_name">
+                                                
+                                        </div>                                        
+                                </div>
+                                <div className="C_skills3_two">
+
+                                </div>
+                        </div>
+                </div>
+            </div>
+            <h1 className="name_skills">Steven Blanco</h1>
+            <h1 className="proffesion_skills">Sofware Developer</h1>
             <img id="img_top_skills" src="..\Assets\Cave\Separete-Cav.svg" alt="" />
             <img className="image-background-skills" src="..\Assets\Backgrounds\Background-Cave.svg" alt="" />
             <img id="img_down_skills" src="..\Assets\Cave\Separete-Cav.svg" alt="" />
